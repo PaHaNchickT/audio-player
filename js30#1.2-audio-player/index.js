79952eb36bf1cfb1fe4d941bc5310f93ffe63608
Pavel <you@example.com>
2022-02-02 02:18:57 +0300

    feat: add prev and next buttons

--- js30#1.2-audio-player/index.js
+++ js30#1.2-audio-player/index.js
@@ -1,8 +1,13 @@
+import songlist from './songs.js';
+
 const audio = new Audio();
 const player = document.querySelector('.btn-play')
 const bar = document.querySelector('.bar-progress')
 const progress = document.querySelector('.range')
 const coordPlayer = player.getBoundingClientRect()
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
 let coordLeft
 let isPlay = false
 let range
@@ -14,8 +19,14 @@ let min = 0
 let sec = 0
 let durmin = 0
 let dursec = 0
+let song = 1
+let songsNumber = 0
 
-audio.src = 'assets/audio/Anacondaz.mp3';
+for (let keys in songlist) {
+    songsNumber = songsNumber + 1
+}
+
+audio.src = `assets/audio/${song}.mp3`;
 
 for (let keys in coordPlayer) {
     coordLeft = coordPlayer['left']
@@ -29,9 +40,9 @@ audio.addEventListener("timeupdate", function () {
         }
     }
     if (isPlay === false) {
-        bar.style.left = `calc(${time/duration*100}% - 5.5px)`
+        bar.style.left = `calc(${time / duration * 100}% - 5.5px)`
     } else {
-        bar.style.left = `calc(${currentTime/duration*100}% - 5.5px)`
+        bar.style.left = `calc(${currentTime / duration * 100}% - 5.5px)`
     }
     duration = audio.duration;
     currentTime = audio.currentTime;
@@ -39,25 +50,25 @@ audio.addEventListener("timeupdate", function () {
     progress.value = range
     //current time
     sec = Math.trunc(currentTime)
-    if ((sec >= 0)&&(sec <= 9)) {
+    if ((sec >= 0) && (sec <= 9)) {
         sec = `0${sec}`
-    }  
+    }
     if (sec >= 60) {
-        min = Math.trunc(currentTime/60)
-        sec = Math.trunc(currentTime%60)
-        if ((sec >= 0)&&(sec <= 9)) {
+        min = Math.trunc(currentTime / 60)
+        sec = Math.trunc(currentTime % 60)
+        if ((sec >= 0) && (sec <= 9)) {
             sec = `0${sec}`
-        }     
+        }
     }
     document.querySelector('.current-time').textContent = `${min}:${sec}`
     //duration time
     dursec = Math.trunc(duration);
     if (dursec >= 60) {
-        durmin = Math.trunc(duration/60)
-        dursec = Math.trunc(duration%60)
-        if ((sec >= 0)&&(sec <= 9)) {
-            sec = `0${sec}`
-        }  
+        durmin = Math.trunc(duration / 60)
+        dursec = Math.trunc(duration % 60)
+        if ((dursec >= 0) && (dursec <= 9)) {
+            dursec = `0${dursec}`
+        }
     }
     document.querySelector('.duration-time').textContent = `${durmin}:${dursec}`
 });
@@ -81,3 +92,30 @@ function playAudio() {
 }
 
 player.addEventListener('click', playAudio);
+
+//prev and next
+
+function prevSong() {
+    song = song - 1
+    audio.src = `assets/audio/${song}.mp3`;
+    audio.play();
+    audio.addEventListener('error', function () {
+        song = songsNumber-1
+        audio.src = `assets/audio/${song}.mp3`;
+        audio.play();
+    }, false);
+}
+
+function nextSong() {
+    song = song + 1
+    audio.src = `assets/audio/${song}.mp3`;
+    audio.play();
+    audio.addEventListener('error', function () {
+        song = 0
+        audio.src = `assets/audio/${song}.mp3`;
+        audio.play();
+    }, false);
+}
+
+prev.addEventListener('click', prevSong);
+next.addEventListener('click', nextSong);
---
+++ js30#1.2-audio-player/songs.js
@@ -0,0 +1,6 @@
+const songlist = {
+    '0': 'Anacondaz=Пусть они умрут=Kids',
+    '1': 'Три дня дождя=Красота=Eyes',
+    '2': 'Linkin park=Numb=Meteora'
+}
+export default songlist;
