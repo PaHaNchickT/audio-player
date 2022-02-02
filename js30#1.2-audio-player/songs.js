e366671a50ee8eef0ec6a31de21afd2428f1f5d6
Pavel <you@example.com>
2022-02-02 15:09:11 +0300

    feat: add another songs

--- js30#1.2-audio-player/songs.js
+++ js30#1.2-audio-player/songs.js
@@ -1,7 +1,9 @@
 const songlist = {
-    '0': 'Anacondaz=Пусть они умрут=Kids',
+    '0': 'Anacondaz=Когда-нибудь=Recall',
     '1': 'Три дня дождя=Перезаряжай=Eyes',
     '2': 'Linkin park=Numb=Meteora',
-    '3': 'Noize MC=Миокард=Exit'
+    '3': 'Noize MC=Миокард=Exit',
+    '4': 'Matthew Perryman Jones=Living in the shadows=Shadows',
+    '5': 'Nizkiz=Лiрика=Lirika'
 }
 export default songlist;
