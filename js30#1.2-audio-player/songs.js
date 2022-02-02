337e4b0a38357c1b2eca5c65addb2d0787656ec8
Pavel <you@example.com>
2022-02-02 14:29:51 +0300

    feat: add NoizeMC song

--- js30#1.2-audio-player/songs.js
+++ js30#1.2-audio-player/songs.js
@@ -1,6 +1,7 @@
 const songlist = {
     '0': 'Anacondaz=Пусть они умрут=Kids',
     '1': 'Три дня дождя=Перезаряжай=Eyes',
-    '2': 'Linkin park=Numb=Meteora'
+    '2': 'Linkin park=Numb=Meteora',
+    '3': 'Noize MC=Миокард=Exit'
 }
 export default songlist;
